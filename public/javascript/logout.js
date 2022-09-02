6e65d7d02d1ec6cbb46ec0711fe31e36508b59d3
Aaron Mandel <aamandel@ucsc.edu>
2022-09-02 14:43:13 -0700

    added basic login, signup, & signout frontend

---
+++ public/javascript/logout.js
@@ -0,0 +1,16 @@
+// delete cookie data if user logs out
+async function logout() {
+    const response = await fetch('/api/users/logout', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+// select logout button by id
+document.querySelector('#logout').addEventListener('click', logout);
--- server.js
+++ server.js
@@ -11,6 +11,20 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// express session and sequelizestore
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+app.use(session(sess));
+
 //express handlebars setup
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});

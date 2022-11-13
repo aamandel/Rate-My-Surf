744dd6722fc0dbbf8ba3a337e31551b4db24baf6
Aaron Mandel <aamandel@ucsc.edu>
2022-11-13 13:13:03 -0800

    updated connection to JawsDB

--- config/connection.js
+++ config/connection.js
@@ -7,13 +7,17 @@ require('dotenv').config();
 // create connection to rms database
 let sequelize;
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL, process.env.JAWSDB_USER, process.env.JAWSDB_PW, {
+    console.log("connecting to JawsDB");
+    sequelize = new Sequelize(process.env.JAWSDB_DB, process.env.JAWSDB_USER, process.env.JAWSDB_PW, {
+        host: process.env.JAWSDB_HOST,
         dialect: 'mysql',
+        port: 3306,
         dialectOptions: {
             multipleStatements: true
         }
     });
 } else {
+    console.log("connecting to localhost");
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',

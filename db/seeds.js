04f7ab825bbb34345b3fdffc5d045d5589ce9092
Aaron Mandel <aamandel@ucsc.edu>
2022-11-13 13:19:14 -0800

    updated seeds.js for JawsDB

--- db/seeds.js
+++ db/seeds.js
@@ -2,6 +2,7 @@ const sequelize = require("../config/connection");
 // routes are required to show sequelize the models
 const routes = require('../controllers');
 const fs = require("fs");
+require('dotenv').config();
 
 const schemaQuery = fs.readFileSync("db/schema.sql", {
     encoding: "utf-8",
@@ -11,11 +12,24 @@ const seedsQuery = fs.readFileSync("db/seeds.sql", {
     encoding: "utf-8",
 })
 
-// initialize the empty rms_db database
-sequelize.query(schemaQuery).then(() => {
-    // then sync the models using sequelize
-    sequelize.sync({ force: true }).then(() => {
-        // then seed the datase
-        sequelize.query(seedsQuery);
+// if JawsDB is available
+if (process.env.JAWSDB_URL) {
+    // use the database
+    sequelize.query('USE' + process.env.JAWSDB_DB).then(() => {
+        // then sync the models using sequelize
+        sequelize.sync({ force: true }).then(() => {
+            // then seed the datase
+            sequelize.query(seedsQuery);
+        });
     });
-});
+} else {
+    // initialize the empty rms_db database
+    sequelize.query(schemaQuery).then(() => {
+        // then sync the models using sequelize
+        sequelize.sync({ force: true }).then(() => {
+            // then seed the datase
+            sequelize.query(seedsQuery);
+        });
+    });
+}
+
